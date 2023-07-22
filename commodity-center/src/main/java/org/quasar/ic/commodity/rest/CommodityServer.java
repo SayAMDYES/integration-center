781188568a24bb8347e008a1e2e59d1ba1522603
Quasar <1042389894@qq.com>
2023-07-22 17:15:25 +0800

    :sparkles: Init project

---
+++ commodity-center/src/main/java/org/quasar/ic/commodity/rest/CommodityServer.java
@@ -0,0 +1,16 @@
+package org.quasar.ic.commodity.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Quasar
+ * @version 1.0.0
+ * @since 2023/7/22 17:33
+ */
+@SpringBootApplication
+public class CommodityServer {
+    public static void main(String[] args) {
+        SpringApplication.run(CommodityServer.class, args);
+    }
+}
