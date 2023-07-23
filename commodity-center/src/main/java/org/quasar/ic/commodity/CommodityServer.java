bab74dde932216c2f5a8355c11a3f9b36ac3c7b5
Quasar <1042389894@qq.com>
2023-07-23 17:52:16 +0800

    :sparkles: Configure scan packages

--- commodity-center/src/main/java/org/quasar/ic/commodity/CommodityServer.java
+++ commodity-center/src/main/java/org/quasar/ic/commodity/CommodityServer.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0.0
  * @since 2023/7/22 17:33
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.quasar.ic")
 public class CommodityServer {
     public static void main(String[] args) {
         SpringApplication.run(CommodityServer.class, args);
