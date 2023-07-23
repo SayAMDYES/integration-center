bb59f22cf86595d69debd7dee0829ddc743cceee
Quasar <1042389894@qq.com>
2023-07-23 17:14:31 +0800

    :sparkles: Add main and application.yml

--- commodity-center/src/main/java/org/quasar/ic/commodity/CommodityServer.java
+++ commodity-center/src/main/java/org/quasar/ic/commodity/CommodityServer.java
@@ -1,4 +1,4 @@
-package org.quasar.ic.commodity.rest;
+package org.quasar.ic.commodity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
