14170a78d3051cf0c13eaf889064907fb6f1803c
Quasar <1042389894@qq.com>
2023-07-23 17:17:40 +0800

    :sparkles: Ignore cors

---
+++ commodity-center/src/main/java/org/quasar/ic/commodity/config/CorsConfiguration.java
@@ -0,0 +1,29 @@
+package org.quasar.ic.commodity.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author Quasar
+ * @version 1.0.0
+ * @since 2023/7/23 17:16
+ */
+@Configuration
+public class CorsConfiguration extends WebMvcConfigurationSupport {
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders(
+                        "access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options")
+                .allowCredentials(false).maxAge(3600);
+        super.addCorsMappings(registry);
+    }
+}
